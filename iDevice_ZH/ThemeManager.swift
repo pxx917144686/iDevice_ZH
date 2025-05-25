962cf0dfece8e3f48be0ff7d2499c06af2396059
pxx917144686 <pxx917144686@163.com>
2025-05-25 20:41:49 +0800

    Update ThemeManager.swift

--- iDevice_ZH/ThemeManager.swift
+++ iDevice_ZH/ThemeManager.swift
@@ -4,56 +4,72 @@ import Combine
 class ThemeManager: ObservableObject {
     static let shared = ThemeManager()
     
+    private let accentColorKey = "accentColorKey"
+    private let greenColorKey = "greenColorKey"
+    
     @Published var accentColor: Color {
         didSet {
-            UserDefaults.standard.set(UIColor(accentColor).cgColor.components, forKey: "accentColor")
+            saveColorToUserDefaults(accentColor, forKey: accentColorKey)
         }
     }
     
     @Published var greenColor: Color {
         didSet {
-            UserDefaults.standard.set(UIColor(greenColor).cgColor.components, forKey: "greenColor")
+            saveColorToUserDefaults(greenColor, forKey: greenColorKey)
         }
     }
     
-    @Published var showColorPicker: Bool = false
-    
-    private init() {
-        // 从UserDefaults加载保存的颜色，如果没有则使用默认颜色
-        if let components = UserDefaults.standard.array(forKey: "accentColor") as? [CGFloat],
-           components.count >= 4 {
-            self.accentColor = Color(.sRGB, red: components[0], 
-                                    green: components[1], 
-                                    blue: components[2], 
-                                    opacity: components[3])
-        } else {
-            self.accentColor = ToolkitColors.accent
-        }
-        
-        if let components = UserDefaults.standard.array(forKey: "greenColor") as? [CGFloat],
-           components.count >= 4 {
-            self.greenColor = Color(.sRGB, red: components[0], 
-                                   green: components[1], 
-                                   blue: components[2], 
-                                   opacity: components[3])
-        } else {
-            self.greenColor = ToolkitColors.green
-        }
-    }
-    
-    func resetToDefaults() {
-        accentColor = ToolkitColors.accent
-        greenColor = ToolkitColors.green
-        UserDefaults.standard.removeObject(forKey: "accentColor")
-        UserDefaults.standard.removeObject(forKey: "greenColor")
-    }
-    
-    // 用于替换应用中使用的颜色
     var currentAccentColor: Color {
         return accentColor
     }
     
     var currentGreenColor: Color {
         return greenColor
+    }
+    
+    private init() {
+        // 从UserDefaults加载保存的颜色，如果没有则使用默认值
+        self.accentColor = loadColorFromUserDefaults(forKey: accentColorKey) ?? ToolkitColors.accent
+        self.greenColor = loadColorFromUserDefaults(forKey: greenColorKey) ?? ToolkitColors.green
+    }
+    
+    func resetToDefaults() {
+        accentColor = ToolkitColors.accent
+        greenColor = ToolkitColors.green
+        
+        // 从UserDefaults中删除保存的颜色
+        UserDefaults.standard.removeObject(forKey: accentColorKey)
+        UserDefaults.standard.removeObject(forKey: greenColorKey)
+    }
+    
+    private func saveColorToUserDefaults(_ color: Color, forKey key: String) {
+        let uiColor = UIColor(color)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        let colorData = [
+            "red": red,
+            "green": green,
+            "blue": blue,
+            "alpha": alpha
+        ]
+        
+        UserDefaults.standard.set(colorData, forKey: key)
+    }
+    
+    private func loadColorFromUserDefaults(forKey key: String) -> Color? {
+        guard let colorData = UserDefaults.standard.dictionary(forKey: key) as? [String: CGFloat],
+              let red = colorData["red"],
+              let green = colorData["green"],
+              let blue = colorData["blue"],
+              let alpha = colorData["alpha"] else {
+            return nil
+        }
+        
+        return Color(.displayP3, red: red, green: green, blue: blue, opacity: alpha)
     }
 }
